5a58e5794084b26fe07412627c9e8237797f758b
Lucas <lucasefernandes333@gmail.com>
2026-05-02 01:42:28 -0300

    feat: random cats on dashboard xd

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,20 @@
+local M = {}
+
+local ascii_dir = vim.fn.stdpath("config") .. "/asciis"
+function M.random_header()
+	  local files = vim.fn.glob(ascii_dir .. "/*", false, true)
+
+	  if #files == 0 then
+	    return { "Neovim" }
+	  end
+
+	  math.randomseed(os.time())
+
+	  local file = files[math.random(#files)]
+	  return vim.fn.readfile(file)
+end
+
+
 return {
 	{
 	  "goolord/alpha-nvim",
@@ -5,23 +22,8 @@ return {
 	  config = function()
 	    local alpha = require("alpha")
 	    local dashboard = require("alpha.themes.dashboard")
-
-	    dashboard.section.header.val = vim.split([[
-		  ,-.       _,---._ __  / \
-		 /  )    .-'       `./ /   \  meooow
-		(  (   ,'            `/    /|
-		 \  `-"             \'\   / |
-		  `.              ,  \ \ /  |
-		   /`.          ,'-`----Y   |
-		  (            ;        |   '
-		  |  ,-.    ,-'         |  /
-	       checking my nvim configs | /
-		  )  |  \  `.___________|/
-		  `--'   `--'
-		]], "\n")
-
+	    dashboard.section.header.val = M.random_header()
 	    dashboard.section.buttons.val = {}
-
 	    alpha.setup(dashboard.opts)
 	  end
 	}
