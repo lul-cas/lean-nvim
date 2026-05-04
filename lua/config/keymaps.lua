4fa3a96729c7ef97d3f052abbec6e4a588b0df76
Lucas <lucasefernandes333@gmail.com>
2026-05-04 11:54:17 -0300

    feat: improve keymaps for buffer jump and indentation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,17 +4,16 @@ local map = vim.keymap.set
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open file explorer" })
-
 
 map("n", "<C-s>", "<cmd>w<cr>", { desc = "saves current buffer" })
 map("n", "<C-q>", "<cmd>q<cr>", { desc = "quits current buffer" })
 
 map('i', '<S-Tab>', '<-C-d>', { noremap = true, silent = true, desc = "Shift TAB" })
-map('v', '<S-Tab>', '<', { noremap = true, silent = true, desc = "Shift TAB" })
-map('n', '<S-Tab>', '<<', { noremap = true, silent = true, desc = "Shift TAB" })
+map("v", "<Tab>", ">gv", { desc = "Indent right" })
+map('v', '<S-Tab>', '<gv', { noremap = true, silent = true, desc = "Shift TAB" })
 
 map('i', '<C-z>', '<Esc>u', {noremap = true, silent = true, desc = "UNDO" })
 map('i', '<C-y>', '<C-r>', {noremap = true, silent = true, desc = "REDO" })
@@ -38,9 +37,6 @@ end, { desc = "Open terminal below" })
 
 map("t", "<C-x>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 map("n", "<leader>w", "<C-w>w", { desc = "Cycle windows" })
-
-map("v", "<Tab>", ">gv", { desc = "Indent right" })
-map("v", "<S-Tab>", "<gv", { desc = "Indent left" })
 
 map("n", "xh", "<C-w>h", { desc = "Go to left window" })
 map("n", "xj", "<C-w>j", { desc = "Go to down window" })
